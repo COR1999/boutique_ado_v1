75a47caa105adeab3afce0e2132e6a5ffa2769dd
COR1999 <CIAN.OROURKE@GMAIL.COM>
2020-08-13 17:47:57 +0000

    added product searching!

--- products/views.py
+++ products/views.py
@@ -5,23 +5,27 @@ from django.db.models import Q
 # Create your views here.
 
 def all_products(request):
-    """A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
 
     if request.GET:
         if 'q' in request.GET:
-            query = request.GET["q"]
+            query = request.GET['q']
             if not query:
-                message.error(request, "You didnt enter any search criteria!")
-                return redirect(reverse("products"))
-
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        "products": products,
+        'products': products,
+        'search_term': query,
     }
 
-    return render(request, "products/products.html", context)
+    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
