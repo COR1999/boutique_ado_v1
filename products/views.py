2553a6c898e8c17c3992ed64c99ca48cb353e073
COR1999 <CIAN.OROURKE@GMAIL.COM>
2020-08-13 00:06:58 +0000

    sharted to do queries video

--- products/views.py
+++ products/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 def all_products(request):
     """A view to show all products, including sorting and search queries"""
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET["q"]
+            if not query:
+                message.error(request, "You didnt enter any search criteria!")
+                return redirect(reverse("products"))
+
 
     context = {
         "products": products,
